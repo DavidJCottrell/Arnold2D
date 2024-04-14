b7e6a908d274121d501ca8bd74d7d7f97a02c993
David Cottrell <davidjcottrell@icloud.com>
2024-04-14 21:40:17 +0100

    Remove player texture again

--- Engine/Map.h
+++ Engine/Map.h
@@ -7,23 +7,24 @@
 #include <SDL2/SDL_image.h>
 #include "TextureManager.h"
 
-class Map
-{
+class Map {
 public:
-    Map(SDL_Renderer *renderer);
+    explicit Map(SDL_Renderer *renderer);
+
     ~Map();
 
     void LoadMap(int mapArr[20][25]);
+
     void DrawMap(SDL_Renderer *renderer);
 
 private:
-    SDL_Rect src, dest;
+    SDL_Rect src{}, dest{};
 
     SDL_Texture *dirt;
     SDL_Texture *grass;
     SDL_Texture *water;
 
-    int map[20][25];
+    int map[20][25]{};
 };
 
 #endif
--- Entities/Player/Player.cpp
+++ Entities/Player/Player.cpp
@@ -1,65 +1,69 @@
 #include "Player.h"
 
-void Player::registerMovementKey(SDL_Keycode key, bool isHeld)
-{
+void Player::registerMovementKey(SDL_Keycode key, bool isHeld) {
     auto movementKey = movementKeys.find(key);
     if (movementKey != movementKeys.end())
         movementKey->second = isHeld;
 }
 
-Projectile Player::spawnProjectile(Vector2D destination)
-{
+Projectile Player::spawnProjectile(Vector2D destination) {
     Vector2D direction = Utils::Geometry::getUnitVector(coordinates, destination);
     return {coordinates, game, direction};
 }
 
-void Player::handleEvents(SDL_Event sdlEvent)
-{
-    switch (sdlEvent.type)
-    {
-    case SDL_KEYDOWN:
-        registerMovementKey(sdlEvent.key.keysym.sym, true);
-        break;
-    case SDL_KEYUP:
-        registerMovementKey(sdlEvent.key.keysym.sym, false);
-        break;
-    case SDL_MOUSEBUTTONDOWN:
-    {
-        int mouseX = 0, mouseY = 0;
-        SDL_GetMouseState(&mouseX, &mouseY);
-        game->addEntity(spawnProjectile({(float)mouseX, (float)mouseY}));
-        MessageHandler::getInstance().SendMsg("Pew");
-        Utils::Audio::playSound(gunSound);
-        break;
-    }
-    default:
-        break;
+void Player::handleEvents(SDL_Event sdlEvent) {
+    switch (sdlEvent.type) {
+        case SDL_KEYDOWN:
+            registerMovementKey(sdlEvent.key.keysym.sym, true);
+            break;
+        case SDL_KEYUP:
+            registerMovementKey(sdlEvent.key.keysym.sym, false);
+            break;
+        case SDL_MOUSEBUTTONDOWN: {
+            int mouseX = 0, mouseY = 0;
+            SDL_GetMouseState(&mouseX, &mouseY);
+            game->addEntity(spawnProjectile({(float) mouseX, (float) mouseY}));
+            MessageHandler::getInstance().SendMsg("Pew");
+            Utils::Audio::playSound(gunSound);
+            break;
+        }
+        default:
+            break;
     }
 }
 
-void Player::render(SDL_Renderer *renderer)
-{
-    int const scale = 3;
+void Player::render(SDL_Renderer *renderer) {
+//    int const scale = 3;
+//
+//    SDL_Rect source = {
+//        17,
+//        21,
+//        static_cast<int>(dimensions.x),
+//        static_cast<int>(dimensions.y),
+//    };
+//
+//    SDL_Rect dest = {
+//        static_cast<int>(coordinates.x),
+//        static_cast<int>(coordinates.y),
+//        static_cast<int>(dimensions.x * scale),
+//        static_cast<int>(dimensions.y * scale),
+//    };
+//
+//    TextureManager::DrawTexture(playerTex, renderer, source, dest);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-    SDL_Rect source = {
-        17,
-        21,
-        static_cast<int>(dimensions.x),
-        static_cast<int>(dimensions.y),
+    SDL_Rect shape = {
+            (int) coordinates.x,
+            (int) coordinates.y,
+            (int) dimensions.x,
+            (int) dimensions.y,
     };
+    SDL_RenderFillRect(renderer, &shape);
 
-    SDL_Rect dest = {
-        static_cast<int>(coordinates.x),
-        static_cast<int>(coordinates.y),
-        static_cast<int>(dimensions.x * scale),
-        static_cast<int>(dimensions.y * scale),
-    };
-
-    TextureManager::DrawTexture(playerTex, renderer, source, dest);
+    SDL_RenderFillRect(renderer, &shape);
 }
 
-void Player::update(double deltaTime)
-{
+void Player::update(double deltaTime) {
     // if (health <= 0)
     // {
     //     MessageHandler::getInstance().SendMsg("Game Over");
@@ -68,11 +72,9 @@ void Player::update(double deltaTime)
 
     Geometry::Vector2D moveAmount = {0.0f, 0.0f};
 
-    for (auto &movementKey : movementKeys)
-    {
+    for (auto &movementKey: movementKeys) {
         // Key is being pressed
-        if (movementKey.second)
-        {
+        if (movementKey.second) {
             if (movementKey.first == SDLK_w)
                 moveAmount.y -= 1.0f;
             if (movementKey.first == SDLK_a)
@@ -86,11 +88,10 @@ void Player::update(double deltaTime)
 
     moveAmount = Geometry::normalise(moveAmount);
 
-    coordinates.x += (float)(moveAmount.x * movementSpeed * deltaTime);
-    coordinates.y += (float)(moveAmount.y * movementSpeed * deltaTime);
+    coordinates.x += (float) (moveAmount.x * movementSpeed * deltaTime);
+    coordinates.y += (float) (moveAmount.y * movementSpeed * deltaTime);
 }
 
-void Player::takeDamage(double damage) const
-{
+void Player::takeDamage(double damage) const {
     health - damage;
 }
